06e27114af10d28c7f1db8fbe115df6e44bbd831
Jamie Hand <hand.jamie@gmail.com>
2016-06-22 16:57:48 -0700

    add bodyparser for isitup

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ app.get('/sayhi', function(request, response) {
   response.send({"text": "Hello world!"});
 });
 
+
+/* bodyParser is for isitup functionality, for interpreting the request */
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
 app.post('/isitup', function(request, response) {
   var command = request.command
   var text = request.text
