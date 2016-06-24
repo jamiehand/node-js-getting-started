eb74ad735a3a4be82548a72b114fdb74e285bf29
Jamie Hand <hand.jamie@gmail.com>
2016-06-24 13:35:46 -0700

    Add Twitter streaming from tracking NBC tweets

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post('/isitup', function(request, response) {
   } // end if-else that checks whether token is correct
 });
 
-var sendToWebHook = function(req, res, payload) {
+var sendToWebHookWithResponse = function(req, res, payload) {
   var options = {
     method: 'POST',
     url: process.env.WEBHOOK_URL,
@@ -143,26 +143,48 @@ var sendToWebHook = function(req, res, payload) {
 };
 
 app.get('/webhook-event', function(req, res) {
-  sendToWebHook(req, res, '{"text": "hello again!"}');
+  sendToWebHookWithResponse(req, res, '{"text": "hello from /webhook-event!"}');
 });
+
+var sendToWebHookNoResponse = function(payload) {
+  var options = {
+    method: 'POST',
+    url: process.env.WEBHOOK_URL,
+    body: payload
+  };
+
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+};
 
 /* Twitter CLIENT TO SEND TO WEBHOOK_URL */
 
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
+// var params = {screen_name: 'nodejs'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response){
+//   if (!error) {
+//     // sendToWebHookWithResponse(tweets)
+//     console.log(tweets);
+//   }
+// })
 
-  }
-})
+var stream = client.stream('statuses/filter', {track: 'NBC'});
+stream.on('data', function(tweet) {
+  var payload = '{"text": "' + tweet.text + '"}'
+  sendToWebHookNoResponse(payload);
+});
 
-
+stream.on('error', function(error) {
+  throw error;
+});
 
 
 
